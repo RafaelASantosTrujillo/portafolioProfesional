200c24b0c9c113d243da5cc283661747d9539622
Rafael Santos <rast.santos@gmail.com>
2021-02-11 11:17:50 -0600

    Mejore foto perfil estilos para certificados y proyectos

---
+++ js/cursor.js
@@ -0,0 +1,12 @@
+let curs = document.querySelector('.mouseover-bg');
+$('document').ready(function () {
+  $('.mouseover-bg').hide();
+  $(".container-certificates").mousemove(function (e) {
+      console.log(e.pageX + ", " + e.pageY);
+      
+      $('.mouseover-bg').css({ left: e.pageX - 100, top: e.pageY - 100, opacity:1 }).show();
+  });
+  $('.container-certificates').mouseout(function () {
+      $('.mouseover-bg').hide();
+  });
+});
